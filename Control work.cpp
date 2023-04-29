fae8dfbee532f2342b0c9d07b7be9aba9320ec5d
AlexeyCDev <129415405+AlexeyCDev@users.noreply.github.com>
2023-04-29 14:17:33 +0300

    Add files via upload

---
+++ Control work.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double sumDeposit = 0; // сумма депозита
+    int amountMonth = 0; // количество мес€цев
+    double profit = 0; // прибыль
+    double annualRate = 5; // п€ть процентов годовых
+    int daysInYear = 365;
+    int daysInMonth = 30;
+
+    cout << "Enter the deposit amount in dollars: ";
+    cin >> sumDeposit;
+    cout << "Enter the number of months: ";
+    cin >> amountMonth;
+
+    cout << "\nCalculation :\n"<< endl;
+    cout << "Interest per month: ";
+    profit = sumDeposit * (annualRate / 100)  / daysInYear * daysInMonth;
+
+    cout << sumDeposit << " * " << annualRate << "%  / " << daysInYear
+         << " * " << daysInMonth << " = " << profit << "$" << endl;
+
+    cout << "Interest for the entire term: ";
+    profit *= amountMonth;
+    cout << profit << " $"  << endl;
+
+    cout << "The total amount of payment at the end of the term: "
+         << sumDeposit + profit  << " $";
+    cout << endl << endl;
+
+    return 0;
+}
